d349bc9017e2f91d54a8b1f053bee65aa7d55c31
Kristofer Rye <kristofer.rye@gmail.com>
2015-02-03 06:07:01 -0600

    Constants winchTalonPort and lightsRelayPort.

--- src/org/usfirst/frc/team2503/Constants.java
+++ src/org/usfirst/frc/team2503/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 	public static final int leftTalonPort = 0;
 	public static final int rightTalonPort = 1;
 	public static final int slipTalonPort = 2;
+	public static final int winchTalonPort = 3;
+	public static final int lightsRelayPort = 0;
+	
 	public static final double drivePrecisionMultiplier = 0.3;
 	
 	public static final String piUrl = "http://10.25.3.17:5800/data";
