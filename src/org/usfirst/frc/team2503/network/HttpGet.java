59c9fbe3e19fb1e470813da329d3eb4fb94d54d3
Kristofer Rye <kristofer.rye@gmail.com>
2015-02-24 11:47:15 -0600

    Http*: Remove some unnecessary imports.

--- src/org/usfirst/frc/team2503/network/HttpGet.java
+++ src/org/usfirst/frc/team2503/network/HttpGet.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.SocketException;
 import java.net.URL;
