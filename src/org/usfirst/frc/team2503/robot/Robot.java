7629a309f1c2697f2d462eb363cbd366b1953e20
Kristofer Rye <kristofer.rye@gmail.com>
2015-03-21 13:48:22 -0500

    Remove comments.
    They're clogging stuff, need to focus.

--- src/org/usfirst/frc/team2503/robot/Robot.java
+++ src/org/usfirst/frc/team2503/robot/Robot.java
@@ -41,135 +41,51 @@ public class Robot extends IterativeRobot implements Statusable {
 		__status__.put("mode", new JSONObject().put("name", "autonomous").put("status", "initializing"));
 		
 		if(VisionClient.hslImage != null) {
-			/**
-			 * We have the vision! :D
-			 */
-			
 			madCatzDriveBaseDriver.drive(0.0, 0.0);
 		} else {
-			/**
-			 * We're flying blind... :'(
-			 */
 			{
-				/**
-				 * Stop driving.
-				 */
 				madCatzDriveBaseDriver.drive(0.0, 0.0);
-				
-				/**
-				 * Dazzle the crowds with boring lights.
-				 */
 				madCatzDriveBaseDriver.masterLightsController.set(MasterLightsControllerStatus.ALL_ON);
-				
-				/**
-				 * Close the clamp.
-				 */
 				madCatzDriveBaseDriver.clamp.set(ClampStatus.CLOSE);
 				
 				Timer.delay(1.5);
 			}
 
-			/**
-			 * Stop driving.
-			 */
 			{
 				madCatzDriveBaseDriver.drive(0.0, 0.0);
 
-				/**
-				 * Fall asleep while stopping.
-				 */
 				Timer.delay(5.0);
 			}
 			
-			/**
-			 * Winchy winch for a bit
-			 */
 			{
-				/**
-				 * Make darn shure dat clamp be closed.
-				 */
 				madCatzDriveBaseDriver.clamp.set(ClampStatus.CLOSE);
-				
-				/**
-				 * Winch dat [[redacted]]
-				 */
 				madCatzDriveBaseDriver.winch(0.8);
 				
-				/**
-				 * Fall asleep while winching...
-				 * 
-				 * #muchdanger #alsosleepy
-				 */
 				Timer.delay(2.0);
 			}
 			
-			/**
-			 * Turn left.
-			 */
 			{
-				/**
-				 * (Pivot around front-right wheel)
-				 */
 				madCatzDriveBaseDriver.drive(0.5, 0.0);
 				
-				/**
-				 * Fall asleep while turning.
-				 */
 				Timer.delay(1.75);
 			}
 			
-			/**
-			 * Stop turning.
-			 */
 			{
-				/**
-				 * Stop the winch.
-				 */
 				madCatzDriveBaseDriver.winch(0.0);	
-
-				/**
-				 * Zero percent powah all around
-				 */
 				madCatzDriveBaseDriver.drive(0.0, 0.0);
 				
-				/**
-				 * Fall asleep while stopping turning.
-				 */
 				Timer.delay(0.1);
 			}
 			
-			/**
-			 * Drive forward.
-			 */
 			{
-				/**
-				 * Use half of the power to drive drivage down the driving driven range of driving.
-				 */
 				madCatzDriveBaseDriver.drive(0.5, -0.5);
 				
-				/**
-				 * Fall asleep while driving forward.
-				 */
 				Timer.delay(1.9);
 			}
 
-			/**
-			 * Stop it all.
-			 */		
 			{
-				/**
-				 * Stop driving.
-				 */
 				madCatzDriveBaseDriver.drive(0.0, 0.0);
-				
-				/**
-				 * Stop winching.
-				 */	
 				madCatzDriveBaseDriver.winch(0.0);
-				
-				/**
-				 * Dazzle the crowds.
-				 */
 				madCatzDriveBaseDriver.masterLightsController.set(MasterLightsControllerStatus.ALL_SLOW_STROBE);
 			}
 		}
