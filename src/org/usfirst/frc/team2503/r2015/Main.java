370cb923a3b50471699819af6f468295ef84c142
Kristofer Rye <kristofer.rye@gmail.com>
2015-09-08 09:45:27 -0500

    Test the WebSocket stuff.

--- src/org/usfirst/frc/team2503/r2015/Main.java
+++ src/org/usfirst/frc/team2503/r2015/Main.java
@@ -1,8 +1,14 @@
 package org.usfirst.frc.team2503.r2015;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.client.WebSocketClient;
 
 public class Main {
 	public static void main(String[] args) {
+		URI uri = new URI("ws://localhost:5080/");
+		WebSocketClient wsc = new WebSocketClient(uri);
 	}
 }
