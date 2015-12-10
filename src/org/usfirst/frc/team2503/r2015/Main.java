9183f8413bed6f588986a5225e6b503c4b86ee62
Kristofer Rye <kristofer.rye@gmail.com>
2015-12-10 11:12:22 -0600

    Main: Remove unnecessary imports.

--- src/org/usfirst/frc/team2503/r2015/Main.java
+++ src/org/usfirst/frc/team2503/r2015/Main.java
@@ -1,11 +1,5 @@
 package org.usfirst.frc.team2503.r2015;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.client.WebSocketClient;
-
 public class Main {
 	public static void main(String[] args) {
 	}
